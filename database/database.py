d6b39a68d0eb72e1d7859c05f31bb665c113b462
nirajsavaliyait <nirajsavaliyait@gmail.com>
2025-07-23 11:34:33 +0530

    Update DB URL to sync with pgAdmin

--- database/database.py
+++ database/database.py
@@ -1,6 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+
+
+DATABASE_URL = "postgresql://bookstore_db_qsqc_user:M84vZoIuBG5qbucMTMejRzpb3DiQ17rp@dpg-d1vlbgndiees73bpct0g-a.oregon-postgres.render.com/bookstore_db_qsqc"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit= False, autoflush= False, bind=engine)
+
+Base = declarative_base()
 
 
 def get_db():
@@ -9,12 +19,4 @@ def get_db():
         yield db
     finally:
         db.close()
-
-DATABASE_URL = "sqlite:///./books.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
-SessionLocal = sessionmaker(autocommit= False, autoflush= False, bind=engine)
-
-Base = declarative_base()
 
